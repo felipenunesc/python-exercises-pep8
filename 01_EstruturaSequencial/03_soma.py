4ebad257d4638cde89b841077c3fa362ceb68f91
Felipe Nunes de Carvaho <06326904102@sefaz.to.gov.br>
2025-01-21 12:31:01 -0300

    Add task 03

---
+++ 01_EstruturaSequencial/03_soma.py
@@ -0,0 +1,8 @@
+# Faça um Programa que peça dois números e imprima a soma.
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+
+soma = num1 + num2
+
+print(f"A soma de {num1} e {num2} resulta em: {soma}")
